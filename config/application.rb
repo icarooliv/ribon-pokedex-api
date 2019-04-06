ed5b8d38df946f1fae84599075c093cba21c0d58
Icaro Oliveira <icarooliv2@gmail.com>
2019-04-06 20:06:09 -0300

    fixing cors

--- config/application.rb
+++ config/application.rb
@@ -31,12 +31,12 @@ module PokedexApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', headers: :any, methods: [:get, :post, :options]
+    #   end
+    # end
 
 
     config.api_only = true
