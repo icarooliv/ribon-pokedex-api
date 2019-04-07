d5aaa2c073f7c3823675b21a5f6d8e839c414a42
Icaro Oliveira <icarooliv2@gmail.com>
2019-04-07 16:45:55 -0300

    cleans pokemon simple serializer

--- app/serializers/pokemon_serializer.rb
+++ app/serializers/pokemon_serializer.rb
@@ -1,6 +1,4 @@
 class PokemonSerializer < ActiveModel::Serializer
-  attributes :id, :name, :sprite_front_url, :api_url, :evolves_from, :types
-
-  has_many :types
+  attributes :id, :name, :sprite_front_url, :api_url, :evolves_from
   
 end
